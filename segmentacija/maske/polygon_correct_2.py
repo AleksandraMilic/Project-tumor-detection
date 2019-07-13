8a5f443663ffe9abea80357a5fe7be49025354ed
AleksandraMilic <aleksandra.milic02@gmail.com>
2019-07-13 17:38:48 +0200

    First version of patching

--- segmentacija/maske/polygon_correct_2.py
+++ segmentacija/maske/polygon_correct_2.py
@@ -2,51 +2,37 @@ import cv2
 import numpy as np
 from roi_polygon import SetPixels, GetPolygon, preprocess_array, dilation_func
 from histogram import ColorWin
+import random 
+
+gs_vals = [0, 50, 127, 150, 200, 255]
 
 ##################### izmena - bez zasebne f-j dobijanje crnih px
 
-def CreateWindow(image, height, w, PATCH_SIZE):
+def CreateWindow(image, height, width, PATCH_SIZE):
     """kreiranti patch za svaki px i bojiti ga u belu boju, ako belih px ima vise """
 
-    for i in range(height):
-        for j in range(w):
-            if i < height-PATCH_SIZE+1 and j < w-PATCH_SIZE+1: #i[0] > PATCH_SIZE-1, j[1] > PATCH_SIZE-1 #########bez +1
-                win = image[i:i + PATCH_SIZE, j:j + PATCH_SIZE]
-                #print("win",win)
-                #print("img",image)
-                
-                #image[i:i + PATCH_SIZE, j:j + PATCH_SIZE] = 255
+    x_border = 0
+    
+    while x_border < width:
+        y_border = 0
 
-                new_win, color = ColorWin(win, PATCH_SIZE, image) # 
-                
-                #r = 0
-                if color == 'white':
-                    #image[i:i + PATCH_SIZE, j:j + PATCH_SIZE] = np.array(new_win)
-                    
-                    # ## obojiti taj deo slike u belo
-                    
-                    #print((image[i:i + PATCH_SIZE, j:j + PATCH_SIZE]).shape)
-                    #print((np.array(new_win)).shape)
-                    
-                    for p in range(PATCH_SIZE): # +1???
-                        for q in range(PATCH_SIZE):
-                            #print(image[p + PATCH_SIZE][q + PATCH_SIZE])
-                            image[p + PATCH_SIZE][q + PATCH_SIZE] = 255
-                    
-                    
-                    
+        while(y_border < height):
+            image[x_border: x_border + PATCH_SIZE, y_border: y_border + PATCH_SIZE] = random.choice(gs_vals)
+            y_border += PATCH_SIZE
+    
+        x_border += PATCH_SIZE
+ 
     cv2.imshow("i", image)           
     cv2.waitKey(0)
-
+    
     return image
-                
-                #pts_win.append(win)
-                #pts_2.append(i)  
+        
 
-if __name__ == "__main__":
+    
+def main():
     """
     image = cv2.imread(r'D:\\Project-tumor-detection\\slike\\edges\\gsa 806.jpg') # bez parametra 0!!!!!!!!
-    ret1, image = cv2.threshold(image,100,255,cv2.THRESH_BINARY)
+    ret1, image = cv2.threshold(image,100,0,cv2.THRESH_BINARY)
 
     #print(image)
     h = np.size(image, 0)
@@ -60,16 +46,16 @@ if __name__ == "__main__":
 
     for i in range(height):
         for j in range(width):
-            image[i][j] = 255
+            image[i][j] = 0
 
-    image[50,50]=255
-    image[150,50]=255
-    image[50,150]=255
-    image[90,90]=255
-    image[75,75]=255
-    image[75,78]=255
-    image[76,75]=255    
-    image[7,78]=255
+    image[0:50, 0:50] = 0
+    image[50:100, 50:100] = 255
 
-    PATCH_SIZE = 20
-    CreateWindow(image, height, width, PATCH_SIZE)
+    test = image[50:100, 50:100]
+    res = cv2.calcHist([test], [0], None, [2], [0, 256]) #beli dole, crni gore
+    print(res)#.tolist())
+    #PATCH_SIZE = 20
+    #CreateWindow(image, height, width, PATCH_SIZE)
+
+if __name__ == "__main__":
+    main()
