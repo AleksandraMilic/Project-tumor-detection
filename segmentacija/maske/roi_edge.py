1c085c2dc9ccc618f79704a3174b470607b92a92
AleksandraMilic <aleksandra.milic02@gmail.com>
2019-08-18 16:58:59 +0200

    get coefficients

--- segmentacija/maske/roi_edge.py
+++ segmentacija/maske/roi_edge.py
@@ -8,11 +8,13 @@ import glob
 from polygon_correct2 import main, CleanImage
 from polyfit_spline import curve_fit 
 from calc_angle import centroid, calc_angle
+from contours import get_cnt_img
+
 
 warnings.simplefilter('ignore', np.RankWarning)
 np.seterr(over='ignore')
 
-def roi_edge_1(im):
+def roi_edge_1(im, coordinates):
     #clean_im, pts2 = main()
 
     # for filename_1, filename_2 in zip(files_1, files_2):
@@ -22,16 +24,18 @@ def roi_edge_1(im):
     h = np.size(im, 0)
     w = np.size(im, 1)
 
+    pts = coordinates
+
     # cv2.imshow("i", im)
     # cv2.waitKey(0)
 
-    PATCH_SIZE = 10 #### 10
-    im_2, pts2, a = main(PATCH_SIZE, im)
+    # PATCH_SIZE = 10 #### 10
+    # im_2, pts2, a = main(PATCH_SIZE, im)
 
-    ret1, im_2 = cv2.threshold(im_2,100,255,cv2.THRESH_BINARY)
+    # ret1, im_2 = cv2.threshold(im_2,100,255,cv2.THRESH_BINARY)
 
     #print(pts2)
-
+    '''
     pts_new = []
     for px in pts2:
 
@@ -66,10 +70,10 @@ def roi_edge_1(im):
 
     clean_img = CleanImage(im_2,pts_new)
     #    cv2.imwrite(filename, clean_img)
-    cv2.imshow("clean", clean_img)
-    cv2.waitKey(0)
+    # cv2.imshow("clean", clean_img)
+    # cv2.waitKey(0)
 
-
+    '''
     print("curve_fit")
 
 
@@ -84,8 +88,8 @@ def roi_edge_1(im):
 
 
 
-    center = centroid(pts_new)
-    new_pts = calc_angle(pts_new, center)
+    center = centroid(pts)
+    new_pts = calc_angle(pts, center)
 
 
     ########## REGRESSION ############# 
@@ -93,33 +97,61 @@ def roi_edge_1(im):
 
 
     # POLYFIT SPLINE ##########
-    print(clean_img.shape)
+    # print(clean_img.shape)
 
-    xP, yP = curve_fit(new_pts)
+    xP, yP, coeffs = curve_fit(new_pts)
 
     print("join edge")
     for x, y in zip(xP, yP):
         if x <= h and y <= w: 
-            clean_img[int(x)][int(y)] = 255
+            im[int(x)][int(y)] = 255
 
 
 
     # cv2.imwrite(filename_2, clean_img)
 
-    cv2.imshow("join edge", clean_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow("join edge", im)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-    return clean_img
-# files_1 = glob.glob('D:\\Project-tumor-detection\\slike\\test\\edge-operators\\canny\\*.jpg')
+    return im, coeffs
+
+
+
+
+files_1 = glob.glob('D:\\Project-tumor-detection\\slike\\test\\edge-operators\\canny\\*.jpg')
 # files_2 = glob.glob('D:\\Project-tumor-detection\\slike\\test\\roi\\canny\\*.jpg')
 if __name__ == "__main__":
-
     # filename_1 = 'D:\\Project-tumor-detection\\slike\\test\\edge-operators\\canny\age 40, m2-2.jpeg'
     # filename_2 = 'D:\\Project-tumor-detection\\slike\\test\\roi\\canny\\age 40, m2-2.jpeg'
-    files_1 = glob.glob('D:\\Project-tumor-detection\\slike\\test\\roi2\\*.jpg')
-    files_2 = glob.glob('D:\\Project-tumor-detection\\slike\\test\\hough-roi\\*.jpg')
-    for filename_1 in files_1:
-        im = cv2.imread(filename_1)
-        im = roi_edge_1(im)
-        cv2.imwrite(filename_1, im)
+    # files_1 = glob.glob('D:\\Project-tumor-detection\\slike\\test\\roi2\\*.jpg')
+    # files_2 = glob.glob('D:\\Project-tumor-detection\\slike\\test\\hough-roi\\*.jpg')
+    files = glob.glob('D:\\Project-tumor-detection\\slike\\test\\hough-roi\\roi-2\\masks\\*.jpg')
+    for filename_1 in files:
+        im = cv2.imread(filename_1,0)
+
+        kernel = np.ones((3,3),np.uint8)
+        im = cv2.erode(im,kernel,iterations = 1)
+        
+
+        h = np.size(im, 0)
+        w = np.size(im, 1)
+
+    #     im = roi_edge_1(im)
+        # cv2.imwrite(filename_1, im)
+
+    # im = cv2.imread('D:\\Project-tumor-detection\\slike\\test\\roi2\\5017.jpg',0)
+        image2 = get_cnt_img(im)
+        coordinates = []
+
+        for x in range(h):
+            for y in range(w):
+                if image2[x,y] == 255:
+                    coordinates.append([x,y])
+
+        print('image2', image2)
+        print('coordinates', coordinates)
+
+        im, coeffs = roi_edge_1(image2, coordinates)
+        
+    
